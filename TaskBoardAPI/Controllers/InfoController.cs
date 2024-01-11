4f08b20860d0a9c46ee2c271f515e0aa4c155e84
sharadika <madu.sharadika@gmail.com>
2024-01-11 22:17:59 +0530

    Add project files.

---
+++ TaskBoardAPI/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskBoardAPI.Controllers
+{
+    [Route("api/info")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult GetHealth()
+        {
+            return Ok(true);
+        }
+
+    }
+}
